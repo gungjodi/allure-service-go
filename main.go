31780d7071474269b3fef4f68ee90b86bf4e6f8d
Gung Jodi <agung.pratama@dana.id>
2023-10-26 00:10:12 +0800

    chore: add main file

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"net/http"
 	"os"
-	reports "osp-allure/routers"
+	"osp-allure/routers"
 	"time"
+
+	_ "osp-allure/docs"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,22 +18,40 @@ func init() {
 	if err != nil {
 		log.Error().AnErr("Error loading .env file", err)
 	}
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Timestamp().Logger()
 }
 
+// @title Gin Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Timestamp().Logger()
+	gin.SetMode(os.Getenv("GIN_MODE"))
 	app := gin.Default()
 	app.MaxMultipartMemory = 512 << 20
-	router := app.Group(os.Getenv("BASE_PATH"))
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, map[string]any{
-			"message": "Hello World",
-		})
-	})
+	base := app.Group("/")
+	{
+		BindAllRouters(base)
+	}
 
-	reports.ProjectsRouters(router)
-	reports.ReportsRouters(router)
+	sub := app.Group(os.Getenv("BASE_PATH"))
+	{
+		BindAllRouters(sub)
+	}
 
 	app.Run("0.0.0.0:5050")
+}
+
+func BindAllRouters(group *gin.RouterGroup) {
+	routers.GeneralRouters(group)
+	routers.ProjectsRouters(group)
+	routers.ReportsRouters(group)
 }
